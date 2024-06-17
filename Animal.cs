e4919c5c3633f69541a9952dd9d6a16df603a5f0
10980023 <103251044+10980023@users.noreply.github.com>
2024-06-17 17:53:54 +0000

    Add files via upload
    1st commit

---
+++ Animal.cs
@@ -0,0 +1,40 @@
+﻿// Base class
+public class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+// Derived class Dog
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+// Derived class Cat
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal myAnimal = new Animal();
+        Dog myDog = new Dog();
+        Cat myCat = new Cat();
+
+        myAnimal.MakeSound(); // Outputs: Some generic sound
+        myDog.MakeSound();    // Outputs: Bark
+        myCat.MakeSound();    // Outputs: Meow
+    }
+}
