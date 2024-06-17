a96035b3057546c4ce53389dfedb488a69513435
10980023 <103251044+10980023@users.noreply.github.com>
2024-06-17 17:55:20 +0000

    Add files via upload
    3rd commit

---
+++ IMovable.cs
@@ -0,0 +1,35 @@
+﻿// Interface
+public interface IMovable
+{
+    void Move();
+}
+
+// Class Car implementing IMovable
+public class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+// Class Bicycle implementing IMovable
+public class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car();
+        Bicycle bicycle = new Bicycle();
+
+        car.Move();      // Outputs: Car is moving
+        bicycle.Move();  // Outputs: Bicycle is moving
+    }
+}
